9d98ee86d22339bd7e8fba71c1c633a6e8bb88c8
Cody <xosicka@gmail.com>
2021-07-20 16:11:22 -0600

    installed and imported paypalrestsdk

--- website/__init__.py
+++ website/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
+import paypalrestsdk
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
