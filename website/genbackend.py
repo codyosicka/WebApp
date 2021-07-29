b07631632d2f57635144167dc8f195fb1ad2bb67
Cody <xosicka@gmail.com>
2021-07-28 21:46:38 -0600

    added sorting to directory in genbackend

--- website/genbackend.py
+++ website/genbackend.py
@@ -5,15 +5,17 @@ import sys
 
 path = "C:\\Users\\Xaos\\Desktop\\Web App\\uploaded_files"
 directory = os.listdir(path)
+directory_sorted = sorted(directory) # sorts files and subdirectories within the current directory in alphabetic order
 
 files_in_directory = []
 if len(directory) != 0:
-	for file in directory:
+	for file in directory_sorted:
 		files_in_directory.append(file)
 else:
 	exit()
 
 print("There are files in the uploaded_files: ", files_in_directory)
+print()
 
 
 txt_list = []
@@ -26,14 +28,20 @@ for file in files_in_directory:
 	elif ".txt" in file:
 		txt_list.append(file)
 
-print(files_in_directory)
+
+dict_of_files = {csv_xlsx_list[i]: txt_list[i] for i in range(len(csv_xlsx_list))}
+
+
 print(txt_list)
+print()
 print(csv_xlsx_list)
+print()
+print(dict_of_files)
 
 
-#dict_of_regression = {}
-#for file in :
-	#dict_of_regression[f'{file}'] = General.gp_symbolic_regression(file, )
+dict_of_regression = {}
+#for key in dict_of_files:
+	#dict_of_regression['{}'.format(key)] = General.gp_symbolic_regression(file, )
 	#pass
 
 
