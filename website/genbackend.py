90778150d445a42148c61119ecb83b9c91827520
Cody <xosicka@gmail.com>
2021-07-29 15:03:15 -0600

    updated genbackend.py to perform regressions, store results in sql db, and delete used files

--- website/genbackend.py
+++ website/genbackend.py
@@ -14,34 +14,42 @@ if len(directory) != 0:
 else:
 	exit()
 
+print()
 print("There are files in the uploaded_files: ", files_in_directory)
 print()
 
 
 txt_list = []
 csv_xlsx_list = []
+object_list = []
 for file in files_in_directory:
-	if ".csv" in file:
-		csv_xlsx_list.append(file)
-	elif ".xlsx" in file:
-		csv_xlsx_list.append(file)
-	elif ".txt" in file:
+	if file.endswith(".csv"):
+		csv_xlsx_list.append("C:\\Users\\Xaos\\Desktop\\Web App\\uploaded_files\\"+file)
+	elif file.endswith(".xlsx"):
+		csv_xlsx_list.append("C:\\Users\\Xaos\\Desktop\\Web App\\uploaded_files\\"+file)
+	elif file.endswith(".txt"):
 		txt_list.append(file)
+		object_list.append(file.replace(".txt", ""))
 
+read_variables = []
+for file in directory_sorted:
+	if file.endswith(".txt"):
+		open_file = open("C:\\Users\\Xaos\\Desktop\\Web App\\uploaded_files\\"+file)
+		read_variables.append(open_file.read())
+		open_file.close()
 
 dict_of_files = {csv_xlsx_list[i]: txt_list[i] for i in range(len(csv_xlsx_list))}
 
 
-print(txt_list)
-print()
-print(csv_xlsx_list)
-print()
-print(dict_of_files)
+dict_of_regression = {object_list[i]: General.gp_symbolic_regression(data=csv_xlsx_list[i], y_variable=read_variables[i]) for i in range(len(csv_xlsx_list))}
+
+for regression in dict_of_regression:
+	General.uploadto_equations_database(dict_of_regression[regression])
+
+for file in directory_sorted:
+	os.remove("C:\\Users\\Xaos\\Desktop\\Web App\\uploaded_files\\"+file)
 
 
-dict_of_regression = {}
-#for key in dict_of_files:
-	#dict_of_regression['{}'.format(key)] = General.gp_symbolic_regression(file, )
-	#pass
-
-
+connection = General.create_engine("mysql+pymysql://unwp2wrnzt46hqsp:b95S8mvE5t3CQCFoM3ci@bh10avqiwijwc8nzbszc-mysql.services.clever-cloud.com/bh10avqiwijwc8nzbszc")
+table = General.pd.read_sql_query("SELECT * FROM equations_table", connection)
+print(table)
