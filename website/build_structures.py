eb669cbec938c659acd624ea592a256cd295dad1
Cody <xosicka@gmail.com>
2021-08-05 15:50:06 -0600

    updated build_structures.py to include the static_matrix_constructor function

--- website/build_structures.py
+++ website/build_structures.py
@@ -4,5 +4,16 @@ from GeneralPythonCopy.General import General
 # If equations database was updated:
 
 total_structures = General.complete_structures()
-
 print(total_structures)
+
+constructed_matrices = {}
+for structure in range(len(total_structures)):
+	if total_structures[structure].shape[0] != total_structures[structure].shape[1]: # if the number of function is not equal to the number of variables
+		continue
+	else:
+		constructed_matrices[structure] = General.static_matrix_constructor(total_structures[structure])
+
+print(constructed_matrices)
+
+
+#constructed_matrices = General.static_matrix_constructor()
