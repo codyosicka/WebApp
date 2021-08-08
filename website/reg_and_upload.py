5afad0a9a9a61101b102eab68182e4ca3e5cbbf3
Cody <xosicka@gmail.com>
2021-08-08 01:48:52 -0600

    changed paths back to desktop from laptop, updated build_structures.py because General.py was updated

--- website/reg_and_upload.py
+++ website/reg_and_upload.py
@@ -3,8 +3,8 @@ import os
 import sys
 
 
-path = "C:\\Users\\Buff14\\Desktop\\Web App\\uploaded_files"
-#path = "C:\\Users\\Xaos\\Desktop\\Web App\\uploaded_files"
+#path = "C:\\Users\\Buff14\\Desktop\\Web App\\uploaded_files"
+path = "C:\\Users\\Xaos\\Desktop\\Web App\\uploaded_files"
 directory = os.listdir(path)
 directory_sorted = sorted(directory) # sorts files and subdirectories within the current directory in alphabetic order
 
@@ -25,11 +25,11 @@ csv_xlsx_list = []
 object_list = []
 for file in files_in_directory:
 	if file.endswith(".csv"):
-		csv_xlsx_list.append("C:\\Users\\Buff14\\Desktop\\Web App\\uploaded_files\\"+file)
-		#csv_xlsx_list.append("C:\\Users\\Xaos\\Desktop\\Web App\\uploaded_files\\"+file)
+		#csv_xlsx_list.append("C:\\Users\\Buff14\\Desktop\\Web App\\uploaded_files\\"+file)
+		csv_xlsx_list.append("C:\\Users\\Xaos\\Desktop\\Web App\\uploaded_files\\"+file)
 	elif file.endswith(".xlsx"):
-		csv_xlsx_list.append("C:\\Users\\Buff14\\Desktop\\Web App\\uploaded_files\\"+file)
-		#csv_xlsx_list.append("C:\\Users\\Xaos\\Desktop\\Web App\\uploaded_files\\"+file)
+		#csv_xlsx_list.append("C:\\Users\\Buff14\\Desktop\\Web App\\uploaded_files\\"+file)
+		csv_xlsx_list.append("C:\\Users\\Xaos\\Desktop\\Web App\\uploaded_files\\"+file)
 	elif file.endswith(".txt"):
 		txt_list.append(file)
 		object_list.append(file.replace(".txt", ""))
@@ -37,8 +37,8 @@ for file in files_in_directory:
 read_variables = []
 for file in directory_sorted:
 	if file.endswith(".txt"):
-		open_file = open("C:\\Users\\Buff14\\Desktop\\Web App\\uploaded_files\\"+file)
-		#open_file = open("C:\\Users\\Xaos\\Desktop\\Web App\\uploaded_files\\"+file)
+		#open_file = open("C:\\Users\\Buff14\\Desktop\\Web App\\uploaded_files\\"+file)
+		open_file = open("C:\\Users\\Xaos\\Desktop\\Web App\\uploaded_files\\"+file)
 		read_variables.append(open_file.read())
 		open_file.close()
 
@@ -51,8 +51,8 @@ for regression in dict_of_regression:
 	General.uploadto_equations_database(dict_of_regression[regression])
 
 for file in directory_sorted:
-	os.remove("C:\\Users\\Buff14\\Desktop\\Web App\\uploaded_files\\"+file)
-	#os.remove("C:\\Users\\Xaos\\Desktop\\Web App\\uploaded_files\\"+file)
+	#os.remove("C:\\Users\\Buff14\\Desktop\\Web App\\uploaded_files\\"+file)
+	os.remove("C:\\Users\\Xaos\\Desktop\\Web App\\uploaded_files\\"+file)
 
 
 connection = General.create_engine("mysql+pymysql://unwp2wrnzt46hqsp:b95S8mvE5t3CQCFoM3ci@bh10avqiwijwc8nzbszc-mysql.services.clever-cloud.com/bh10avqiwijwc8nzbszc")
