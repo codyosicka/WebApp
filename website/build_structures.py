0799dc407c9f2e686f27e557b174272b2d3bb1f5
Cody <xosicka@gmail.com>
2021-08-09 15:04:47 -0600

    updated build_structures.py to include updates to General Python

--- website/build_structures.py
+++ website/build_structures.py
@@ -10,7 +10,7 @@ print(total_structures)
 causal_results = {}
 for structure in range(len(total_structures)):
 	# Requires that the structure is self-contained (number of functions = number of variables)
-	if total_structures[structure].shape[0] != total_structures[structure].shape[1]: # if the number of function is not equal to the number of variables
+	if total_structures[structure].shape[0] != total_structures[structure].shape[1]: # if the number of functions is not equal to the number of variables
 		continue
 	else:
 		causal_results[structure] = General.static_causal_order(total_structures[structure])
