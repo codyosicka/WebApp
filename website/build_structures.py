f6ff4c4107d23451dd23e966a17964a77bb8f22b
Cody <xosicka@gmail.com>
2021-08-09 15:04:00 -0600

    updated build_structures.py to include updates to General Python

--- website/build_structures.py
+++ website/build_structures.py
@@ -3,18 +3,21 @@ from GeneralPythonCopy.General import General
 
 # If equations database was updated:
 
-total_structures = General.complete_structures()
+total_structures, total_names = General.complete_structures()[0], General.complete_structures()[1]
+print(total_names)
 print(total_structures)
 
-constructed_matrices = {}
+causal_results = {}
 for structure in range(len(total_structures)):
+	# Requires that the structure is self-contained (number of functions = number of variables)
 	if total_structures[structure].shape[0] != total_structures[structure].shape[1]: # if the number of function is not equal to the number of variables
 		continue
 	else:
-		#constructed_matrices[structure] = General.static_self_contained_causal_structure(total_structures[structure])
+		causal_results[structure] = General.static_causal_order(total_structures[structure])
 		pass
 		
-print(constructed_matrices)
+print(causal_results)
+print(causal_results[0])
 
 
 #constructed_matrices = General.static_matrix_constructor()
